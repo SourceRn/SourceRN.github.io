53c80dc28016e06b162b20204e8cca8efaef5bf1
Rammses Nieves <76192169+SourceRn@users.noreply.github.com>
2021-11-15 00:02:38 -0600

    Delete src/js directory

--- src/js/js.js
+++
@@ -1,7 +0,0 @@
-videos = ["adsasd", "asdasd", "asdas"];
-
-function showVideos(arr){
-    document.getElementById("videos").innerHTML = '<video src="'+arr[0]+'"></video> <video src="'+arr[1]+'"></video> <video src="'+arr[2]+'"></video>';
-}
-
-showVideos(videos);
